2a5d4552ee2e17d22216dd5d7d456595136ebb5f
Chad Liu <liu_q@worksap.co.jp>
2016-08-03 12:06:21 +0800

    Added api

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ var controller = Botkit.slackbot({
 controller.on('message_received', function(bot, message) {
     console.log('message: ' );
     console.log(message);
-    console.log('bot: ');
-    console.log(bot);
 });
 
 var bot = controller.spawn({
@@ -29,12 +27,23 @@ app.get('/', function(request, response) {
   response.send('Hello world');
 });
 
-app.get('/slacklog', function(request, response){
+app.get('/slacklog/channels', function(request, response){
     simple_storeage_provider.channels.all(function (err, data){
         response.send(data);
     });
 });
 
+app.get('/slacklog/users', function(request, response){
+    simple_storeage_provider.users.all(function (err, data){
+        response.send(data);
+    });
+});
+
+app.get('/slacklog/teams', function(request, response){
+    simple_storeage_provider.teams.all(function (err, data){
+        response.send(data);
+    });
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
