6912ef0655399c07526207289dfcb2ca31d520e4
Chad Liu <liu_q@worksap.co.jp>
2016-08-03 12:50:01 +0800

    Remove storage and add debug message

--- index.js
+++ index.js
@@ -3,38 +3,37 @@
 var express = require('express');
 var app = express();
 
-var simple_storeage_provider = require('./simple_storeage.js')();
-
 var Botkit = require('botkit');
 var controller = Botkit.slackbot({
-    storage:  simple_storeage_provider
-});
-
-controller.on('message_received', function(bot, message) {
-    console.log('message: ' );
-    console.log(message);
-    bot.reply(message, 'I heard... something!');
 });
 
 // reply to a direct mention - @bot hello
 controller.on('direct_mention',function(bot,message) {
   // reply to _message_ by using the _bot_ object
+  console.log('direct_mention: ');
+  console.log(message);
   bot.reply(message,'I heard you mention me!');
 });
 
 // reply to a direct message
 controller.on('direct_message',function(bot,message) {
   // reply to _message_ by using the _bot_ object
+  console.log('direct_message: ');
+  console.log(message);
   bot.reply(message,'You are talking directly to me');
 });
 
 controller.on('mention',function(bot,message) {
   // reply to _message_ by using the _bot_ object
+  console.log('mention: ');
+  console.log(message);
   bot.reply(message,'Are you mention me');
 });
 
 controller.on('ambient',function(bot,message) {
   // reply to _message_ by using the _bot_ object
+  console.log('ambient message: ');
+  console.log(message);
   bot.reply(message,'You are not talking to me');
 });
 
@@ -51,23 +50,6 @@ app.get('/', function(request, response) {
   response.send('Hello world');
 });
 
-app.get('/slacklog/channels', function(request, response){
-    simple_storeage_provider.channels.all(function (err, data){
-        response.send(data);
-    });
-});
-
-app.get('/slacklog/users', function(request, response){
-    simple_storeage_provider.users.all(function (err, data){
-        response.send(data);
-    });
-});
-
-app.get('/slacklog/teams', function(request, response){
-    simple_storeage_provider.teams.all(function (err, data){
-        response.send(data);
-    });
-});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
