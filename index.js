198e6ba11ec1ee992acc33669004fb35af94dd52
Chad Liu <liu_q@worksap.co.jp>
2016-08-03 14:12:03 +0800

    Add API

--- index.js
+++ index.js
@@ -32,8 +32,10 @@ controller.on('mention',function(bot,message) {
 
 controller.on('ambient',function(bot,message) {
   // reply to _message_ by using the _bot_ object
-  console.log('ambient message: ');
-  console.log(message);
+  console.log('channel id: ' + message.channel);
+  console.log('text: ' + message.text);
+  console.log('user id: ' + message.user);
+
   bot.reply(message,'You are not talking to me');
 });
 
@@ -48,6 +50,18 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   response.send('Hello world');
+});
+
+app.get('/channels', function(request, response) {
+  bot.api.channels.list({'exclude_archived', '1'}, function(err, data){
+    response.send(data);
+  })
+});
+
+app.get('users', function(request, response) {
+  bot.api.users.list({'presence', '1'}, function(err, data){
+    response.send(data);
+  })
 });
 
 app.listen(app.get('port'), function() {
