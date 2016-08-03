811d0d654836652e278de9544aa7f21f8a755e49
Chad Liu <liu_q@worksap.co.jp>
2016-08-03 15:54:52 +0800

    Get user after ready

--- index.js
+++ index.js
@@ -88,29 +88,43 @@ app.get('/channels', function(request, response) {
 
 var users = {};
 
-bot.api.users.list({'presence':'1'}, function(err, data){
-    if (!data.ok || err){
-        console.log('error to get user list');
-        return;
-    }
-    for (var i = 0; i < data.member.length; i++){
-        users[data.member[i].id] = data.member[i].name;
-    }
-});
+function getUsers(callback){
+    bot.api.users.list({'presence':'1'}, function(err, data){
+        if (!data.ok || err){
+            console.log('error to get user list');
+            return;
+        }
+        for (var i = 0; i < data.member.length; i++){
+            users[data.member[i].id] = data.member[i].name;
+        }
+        callback();
+    });
+}
+
+function getChannelMessages(request, response){
+
+  pool.query('select user_id,  text, timestamp from  slack_log where channel_id =$1 order by timestamp desc', 
+    [request.params.channel],
+    function(err, result) {
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      result['channelName'] = request.params.name;
+      response.send(result);
+      for (var i = 0; i < result.rows.length; i++){
+        result.rows[i]['username'] = users[result.rows[i]['user_id']];
+      }
+      response.render('channel', result);
+    });
+}
 
 app.get('/channel/:channel/name/:name', function(request, response) {
-  pool.query('select user_id,  text, timestamp from  slack_log where channel_id =$1 order by timestamp desc', 
-            [request.params.channel],
-            function(err, result) {
-              if (err)
-               { console.error(err); response.send("Error " + err); }
-              result['channelName'] = request.params.name;
-              response.send(result);
-              for (var i = 0; i < result.rows.length; i++){
-                result.rows[i]['username'] = users[result.rows[i]['user_id']];
-              }
-              response.render('channel', result);
-            });
+    if (Object.keys(users).length === 0){
+        getUsers(
+                getChannelMessages(request, response)
+            );
+    }
+    getChannelMessages(request, response);
+  
 });
 
 app.get('/users', function(request, response) {
