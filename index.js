0300ff03085ee0cf15d1ed42a171d9d3bebc5357
Chad Liu <liu_q@worksap.co.jp>
2016-08-03 12:29:43 +0800

    Try message received events

--- index.js
+++ index.js
@@ -13,7 +13,31 @@ var controller = Botkit.slackbot({
 controller.on('message_received', function(bot, message) {
     console.log('message: ' );
     console.log(message);
+    bot.reply(message, 'I heard... something!');
 });
+
+// reply to a direct mention - @bot hello
+controller.on('direct_mention',function(bot,message) {
+  // reply to _message_ by using the _bot_ object
+  bot.reply(message,'I heard you mention me!');
+});
+
+// reply to a direct message
+controller.on('direct_message',function(bot,message) {
+  // reply to _message_ by using the _bot_ object
+  bot.reply(message,'You are talking directly to me');
+});
+
+controller.on('mention',function(bot,message) {
+  // reply to _message_ by using the _bot_ object
+  bot.reply(message,'Are you mention me');
+});
+
+controller.on('ambient',function(bot,message) {
+  // reply to _message_ by using the _bot_ object
+  bot.reply(message,'You are not talking to me');
+});
+
 
 var bot = controller.spawn({
   token: process.env.TOKEN
