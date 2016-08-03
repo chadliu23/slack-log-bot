a3365b6812cd35deef141ff4dcda337c7245476d
Chad Liu <liu_q@worksap.co.jp>
2016-08-03 11:23:25 +0800

    Use botkit to storeage slack log

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+
+var simple_storeage_provider = require('./simple_storeage.js')();
+
+var Botkit = require('botkit');
+var controller = Botkit.slackbot({
+    storage:  simple_storeage_provider
+});
+var bot = controller.spawn({
+  token: process.env.TOKEN
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('Hello world');
+});
+
+app.get('/slacklog', function(request, response){
+    simple_storeage_provider.storage.channels.all(function (err, data){
+        response.send(data);
+    });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ simple_storeage.js
@@ -0,0 +1,64 @@
+
+var Store = require('jfs');
+module.exports = function(config) {
+
+    if (!config) {
+        config = {
+            path: './',
+        };
+    }
+
+    var teams_db = new Store(config.path + '/teams', {saveId: 'id'});
+    var users_db = new Store(config.path + '/users', {saveId: 'id'});
+    var channels_db = new Store(config.path + '/channels', {saveId: 'id'});
+
+    var objectsToList = function(cb) {
+        return function(err, data) {
+            if (err) {
+                cb(err, data);
+            } else {
+                cb(err, Object.keys(data).map(function(key) {
+                    return data[key];
+                }));
+            }
+        };
+    };
+
+    var storage = {
+        teams: {
+            get: function(team_id, cb) {
+                teams_db.get(team_id, cb);
+            },
+            save: function(team_data, cb) {
+                teams_db.save(team_data.id, team_data, cb);
+            },
+            all: function(cb) {
+                teams_db.all(objectsToList(cb));
+            }
+        },
+        users: {
+            get: function(user_id, cb) {
+                users_db.get(user_id, cb);
+            },
+            save: function(user, cb) {
+                users_db.save(user.id, user, cb);
+            },
+            all: function(cb) {
+                users_db.all(objectsToList(cb));
+            }
+        },
+        channels: {
+            get: function(channel_id, cb) {
+                channels_db.get(channel_id, cb);
+            },
+            save: function(channel, cb) {
+                channels_db.save(channel.id, channel, cb);
+            },
+            all: function(cb) {
+                channels_db.all(objectsToList(cb));
+            }
+        }
+    };
+
+    return storage;
+};
