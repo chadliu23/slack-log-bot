318cd5551aac2f3e5ab3836a637a5056ad167bce
Chad Liu <liu_q@worksap.co.jp>
2016-08-04 12:24:48 +0800

    Change text

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var  mustacheExpress = require('mustache-express'); 
 var Pool = require('pg-pool');
 const url = require('url');
-var Botkit = require('botkit');
+var request = require('request');
 var app = express();
   
 
@@ -17,20 +17,45 @@ const config = {
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
-  ssl: true
+  ssl: true,
+  token: process.env.TOKEN
 };
 
 var pool = new Pool(config);
 
+var slackUsers = {};
+var usersData = {};
+var slackChannels = {};
+var channelsData = {};
 
+function updateLists () {
+  request.get({
+      url: 'https://slack.com/api/users.list?token=' + config.token
+  }, function (error, response, body) {
+    var res = JSON.parse(body);
+    usersData = res;
+    console.log('updated:', new Date());
+    res.members.map(function (member) {
+      slackUsers[member.id] = member.name;
+    });
+  });
 
-var controller = Botkit.slackbot({
-});
+  request.get({
+      url: 'https://slack.com/api/channels.list?token=' + config.token
+  }, function (error, response, body) {
+    var res = JSON.parse(body);
+    channelsData = res;
+    res.channels.map(function (channel) {
+      slackChannels[channel.id] = channel.name;
+    });
+  });
 
-var bot = controller.spawn({
-  token: process.env.TOKEN
-}).startRTM();
+  setTimeout(function () {
+    updateLists()
+  }, 10 * 60 * 1000);
+}
 
+updateLists();
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
@@ -44,26 +69,10 @@ app.get('/', function(request, response) {
 });
 
 app.get('/channels', function(request, response) {
-  bot.api.channels.list({'exclude_archived':'1'}, function(err, data){
-    response.render('channelList', data);
-  })
+    response.render('channelList', channelsData);
 });
 
-var users = {};
 
-function getUsers(request, response, callback){
-    bot.api.users.list({'presence':'1'}, function(err, data){
-        if (!data.ok || err){
-            console.log('error to get user list');
-            return;
-        }
-
-        for (var i = 0; i < data.members.length; i++){
-            users[data.members[i].id] = data.members[i].name;
-        }
-        callback(request, response, users);
-    });
-}
 
 var getChannelMessages = function (request, response, users){
 
@@ -73,25 +82,50 @@ var getChannelMessages = function (request, response, users){
       if (err)
        { console.error(err); response.send("Error " + err); }
       result['channelName'] = request.params.name;
+      //  "[-a-zA-Z0-9@:%_\+\.~#?&//=]{2,256}.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?"
+      var url = "[-a-zA-Z0-9@:%_\\+\.~#?&//=]{2,256}\\.[a-z]{2,4}\\b(\\/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)?";
+      var re = new RegExp("<(" + url + ")\\|" + url + ">","gi");
+        
+
       for (var i = 0; i < result.rows.length; i++){
         result.rows[i]['username'] = users[result.rows[i]['user_id']];
+
+        var message = result.rows[i]['text'];
+        /*
+         * Channel ID -> Channel Name
+         * Member ID -> Member Name
+         * decoed URL and remove <, >
+         */
+        message = message.replace(/<#C\w{8}>/g, function (matched) {
+          var channel_id = matched.match(/#(C\w{8})/)[1];
+          return '#' + slackChannels[channel_id];
+        }).replace(/<@U\w{8}>/g, function (matched) {
+          var member_id = matched.match(/@(U\w{8})/)[1];
+          return '@' + slackUsers[member_id];
+        }).replace(re, function (matched, link) {
+          return link = link.replace(/&amp;/g, '&');
+        }).replace(/<http.*?>/g, function (matched){
+          var src = matched.match(/<(http.*?)>/)[1];
+          return "<a href='" + src + "'>" +src+  "</a>";
+        }).replace(/<[^(\/|a)].*?>/g, function (matched){
+          var src = matched.match(/<([^(\/|a)].*?>)/)[1];
+          return src;
+        }).replace(/&lt;/g,'<').replace(/&gt;/g, '>')
+        ;
+
+        result.rows[i]['text'] = message;
       }
       response.render('channel', result);
     });
 }
 
 app.get('/channel/:channel/name/:name', function(request, response) {
-    if (Object.keys(users).length === 0){
-        getUsers(request, response, getChannelMessages);
-    }
-    getChannelMessages(request, response, users);
+    getChannelMessages(request, response, slackUsers);
   
 });
 
 app.get('/users', function(request, response) {
-  bot.api.users.list({'presence':'1'}, function(err, data){
-    response.send(data);
-  })
+  response.send(usersData);
 });
 
 app.listen(app.get('port'), function() {
